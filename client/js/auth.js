abc0447e857c1b9617ca2b42cd40eb47af6e3df3
kate <katezraly@gmail.com>
2024-10-26 16:13:31 -0700

    prepared api calls on front end

--- client/js/auth.js
+++ client/js/auth.js
@@ -1,7 +1,24 @@
 export class Auth {
 
-    constructor() {
-        console.log("hello")
+    constructor(registerButtonId, loginButtonId, forgotButtonId) {
+        document.getElementById(registerButtonId).onclick = this.register.bind(this)
+        document.getElementById(loginButtonId).onclick = this.login.bind(this)
+        document.getElementById(forgotButtonId).onclick = this.forgotPassword.bind(this)
+    }
+
+    register() {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error('Error:', error))
+    }
+
+    login() {
+        console.log("login")
+    }
+
+    forgotPassword() {
+        console.log("forgot password")
     }
 
 }
--- client/js/login.js
+++ client/js/login.js
@@ -1,7 +1,11 @@
 import { Auth } from "./auth.js"
 import { Initializer } from "./initializer.js"
 
+const REGISTER_BUTTON_ID = "registerButton"
+const LOGIN_BUTTON_ID = "loginButton"
+const FORGOT_BUTTON_ID = "forgotButton"
+
 document.addEventListener("DOMContentLoaded", () => {
     Initializer.loadUserMessages()
-    new Auth()
+    new Auth(REGISTER_BUTTON_ID, LOGIN_BUTTON_ID, FORGOT_BUTTON_ID)
 })
