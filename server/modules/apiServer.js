904f3f457262f40d9843106391e239dd841dc533
kate <katezraly@gmail.com>
2024-10-26 14:14:17 -0700

    added connection to database

--- server/modules/apiServer.js
+++ server/modules/apiServer.js
@@ -1,10 +1,17 @@
 const express = require("express")
+const DatabaseConnection = require('./db.js')
 const Pinger = require("./ping.js")
 
 module.exports = class ApiServer {
 
     constructor() {
         this.app = express()
+        this.db = new DatabaseConnection(
+            process.env.MYSQLHOST, 
+            process.env.MYSQLUSER, 
+            process.env.MYSQLPASSWORD, 
+            process.env.MYSQLDATABASE
+        )
         this.pinger = new Pinger()
     }
 
@@ -16,6 +23,11 @@ module.exports = class ApiServer {
     exposeRoutes() {
         this.app.get("/ping1", this.pinger.ping1)
         this.app.get("/ping2", this.pinger.ping2)
+        this.app.get("/users", (req, res) => {
+            const users = this.db.query("SELECT * FROM user;")
+            console.log(users)
+            res.send({ data: users })
+        })
     }
 
 }
