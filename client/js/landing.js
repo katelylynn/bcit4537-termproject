b796038556c80a6025657161b53473e9c3ea759e
kate <katezraly@gmail.com>
2024-10-31 00:26:57 -0700

    created login and landing classes

--- client/js/landing.js
+++ client/js/landing.js
@@ -1,5 +1,24 @@
 import { Initializer } from "./initializer.js"
 
+const GET_ALL_USERS_BUTTON_ID = "getAllUsersButton"
+
+class Landing {
+
+    constructor(getAllUsersButtonId) {
+        document.getElementById(getAllUsersButtonId).onclick = this.getAllUsers
+    }
+
+    // temp, testing calling the server
+    getAllUsers() {
+        fetch("https://bcit4537-termproject-server.up.railway.app/users")
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error('Error:', error))
+    }
+
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     Initializer.loadUserMessages()
+    new Landing(GET_ALL_USERS_BUTTON_ID)
 })
