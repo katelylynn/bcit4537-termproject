a4671a873d47ce907e76877cf1d6cfc519b74934
kate <katezraly@gmail.com>
2024-10-31 00:08:02 -0700

    updated cors requirements

--- server/modules/apiServer.js
+++ server/modules/apiServer.js
@@ -1,5 +1,4 @@
 const express = require("express")
-const cors = require("cors")
 const routes = require("../routes")
 
 module.exports = class ApiServer {
@@ -15,10 +14,11 @@ module.exports = class ApiServer {
     }
     
     setCorsHeaders(_, res, next) {
-        res.header("Access-Control-Allow-Origin", process.env["ACCESS-CONTROL-ALLOW-ORIGIN"]);
-        res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-        res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
-        next();
+        res.header("Access-Control-Allow-Origin", process.env["ACCESS-CONTROL-ALLOW-ORIGIN"])
+        res.header('Access-Control-Allow-Credentials', 'true')
+        res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
+        res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With")
+        next()
     }
 
 }
