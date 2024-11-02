90882eeb0162bd04ab21c9aa7900b913cc0c6ee2
Harrison de Jong <harrisondijon@gmail.com>
2024-11-01 18:51:22 -0700

    Preliminary directories for router service and whisper service. Added structural code withou  logic to carController and whisperController

---
+++ router-service/controllers/whisperController.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs');
+
+const WHISPER_SERVICE_URL = none;
+
+async function transcribeAudio(req, res) {
+    try {
+
+        //POST THE .WAV FILE TO WHISPER 
+       
+    } catch (error) {
+
+    }
+}
+
+module.exports = { transcribeAudio };
