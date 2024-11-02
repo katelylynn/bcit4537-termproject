7fbf3f15d788d0f7d20d17736dedcedd07fa328e
kate <katezraly@gmail.com>
2024-11-02 10:43:33 -0700

    implemented user management functions

--- database-service/modules/userManager.js
+++ database-service/modules/userManager.js
@@ -6,37 +6,61 @@ module.exports = class UserManager {
         this.db = db
     }
 
-    register(req, res) {
-        //
-    }
-
-    login(req, res) {
-        //
-    }
-
     getAllUsers(req, res) {
         // admins only
         this.db.query("SELECT * FROM User;", (err, obj) => {
-            if (err) console.log(err)
+            if (err) res.send(err.message)
             else res.send(JSON.stringify(obj))
         })
     }
 
     getUser(req, res) {
-        // user gets their own info
+        const uid = req.params.userId
+        
+        this.db.query("SELECT * FROM User WHERE id = ?;", [uid], (err, obj) => {
+            if (err) return res.send(err.message)
+            if (obj.length === 0) return res.status(404).send("User not found.")
+            res.json(obj[0])
+        })
     }
 
     patchUser(req, res) {
-        // user updates their own info
-    }
+        const uid = req.params.userId
+        
+        // TEMP: building query dynamically until we decide which fields a user can modify
+        const updates = req.body
+        const fields = Object.keys(updates).map(field => `${field} = ?`).join(", ")
+        const values = Object.values(updates)
 
+        values.push(uid)
+
+        this.db.query(`UPDATE User SET ${fields} WHERE id = ?;`, values, (err) => {
+            if (err) return res.send(err.message)
+            res.send("User updated successfully.")
+        })
+    }
 
     patchUserRole(req, res) {
         // admins only
+
+        const uid = req.params.userId
+        const { role } = req.body
+
+        this.db.query("UPDATE User SET role = ? WHERE id = ?;", [role, uid], (err) => {
+            if (err) return res.send(err.message)
+            res.send("User role updated successfully.")
+        })
     }
 
     deleteUser(req, res) {
         // admins only
+
+        const uid = req.params.userId
+
+        this.db.query("DELETE FROM User WHERE id = ?;", [uid], (err) => {
+            if (err) return res.status(500).send(err.message)
+            res.send("User deleted successfully.")
+        })
     }
 
 }
