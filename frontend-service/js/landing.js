8f6096024c331b21ed03df65f74e2064dc0217b6
kate <katezraly@gmail.com>
2024-11-03 15:01:40 -0800

    connected logout button

--- frontend-service/js/landing.js
+++ frontend-service/js/landing.js
@@ -1,8 +1,10 @@
+import { Auth } from "./auth.js"
 import { AudioManager } from "./audio.js"
 import { Initializer } from "./initializer.js"
 
 const DOM_CONTENT_LOADED = "DOMContentLoaded"
 const DOWNLOAD_LINK_ID = "downloadLink"
+const LOGOUT_BUTTON_ID = "logoutButton"
 const RECORD_BUTTON_ID = "recordButton"
 const STATUS_ID = "status"
 
@@ -17,4 +19,6 @@ class Landing {
 document.addEventListener(DOM_CONTENT_LOADED, () => {
     Initializer.loadUserMessages()
     new Landing()
+
+    document.getElementById(LOGOUT_BUTTON_ID).onclick = Auth.logout
 })
