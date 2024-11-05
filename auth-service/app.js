27f98571d172f8122c52c610a1545250505e0707
JVViacrusis <justin.viacrusis21p@gmail.com>
2024-11-05 00:21:13 -0800

    refactored auth-service to match other microservice code structure

--- auth-service/app.js
+++ auth-service/app.js
@@ -1,32 +1,4 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const AuthService = require('./modules/api.js');
 
-const app = express();
-const SECRET_KEY = 'totallynotakey';
-
-app.use(express.json());
-
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-
-    if (username !== 'admin' || password !== '111') {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
-
-    const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: '60s' });
-    res.cookie('token', token, { httpOnly: true });
-    res.status(200).json({ message: 'Logged in successfully' });
-});
-
-app.post('/logout', (req, res) => {
-    res.cookie('token', '', { httpOnly: true, maxAge: 0 }); // Clear token
-    res.status(200).json({ message: 'Logged out successfully' });
-});
-
-app.use((req, res) => {
-    res.status(404).json({ message: '404 Not Found' });
-});
-
-app.listen(3000, () => {
-    console.log('Authorization service running on http://localhost:3000');
-});
+const authService = new AuthService('totallynotakey');
+authService.start(3000);
