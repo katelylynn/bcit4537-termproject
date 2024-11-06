e3d08f3580ac443cac57a5f5c2f59519f2ce210a
kate <katezraly@gmail.com>
2024-11-05 16:02:17 -0800

    made central location to store router service reference

--- frontend-service/js/admin.js
+++ frontend-service/js/admin.js
@@ -1,3 +1,4 @@
+import { Api } from "./api.js"
 import { Auth } from "./auth.js"
 import { AudioManager } from "./audio.js"
 import { Initializer } from "./initializer.js"
@@ -5,10 +6,12 @@ import { Initializer } from "./initializer.js"
 const API_CONSUMPTION_STATS_ID = "apiConsumptionStats"
 const DOM_CONTENT_LOADED = "DOMContentLoaded"
 const DOWNLOAD_LINK_ID = "downloadLink"
+const ENDPOINTS_PATH = "/api-consumption-endpoints"
 const LOGOUT_BUTTON_ID = "logoutButton"
 const RECORD_BUTTON_ID = "recordButton"
 const STATUS_ID = "status"
 const USER_CONSUMPTION_STATS_ID = "userConsumptionStats"
+const USERS_PATH = "/api-consumption-users"
 
 class Admin {
 
@@ -16,32 +19,27 @@ class Admin {
         new AudioManager(DOWNLOAD_LINK_ID, RECORD_BUTTON_ID, STATUS_ID)
     }
 
-    updateStats(url, stat_id) {
-        fetch(url)
-            .then(response => {
-                return response.json()
-            })
-            .then(data => {
-                let jsonString = '';
-                data.forEach((obj, index) => {
-                    for (const [key, value] of Object.entries(obj)) {
-                        jsonString += `${key.replace(/\d+/, '')}: ${value}<br>`;
-                    }
-                    jsonString += '<br>';
-                });
-                document.getElementById(stat_id).innerHTML = jsonString
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error.message)
-            })
+    updateStats(data, stat_id) {
+        let jsonString = '';
+        data.forEach((obj, index) => {
+            for (const [key, value] of Object.entries(obj)) {
+                jsonString += `${key.replace(/\d+/, '')}: ${value}<br>`;
+            }
+            jsonString += '<br>';
+        });
+        document.getElementById(stat_id).innerHTML = jsonString
     }
 
     updateEndpointStats() {
-        this.updateStats("http://localhost:3000/api/api-consumption-endpoints", API_CONSUMPTION_STATS_ID)
+        Api.callRouterService(ENDPOINTS_PATH, data => {
+            this.updateStats(data, API_CONSUMPTION_STATS_ID)
+        })
     }
 
     updateUserStats() {
-        this.updateStats("http://localhost:3000/api/api-consumption-users", USER_CONSUMPTION_STATS_ID)
+        Api.callRouterService(USERS_PATH, data => {
+            this.updateStats(data, USER_CONSUMPTION_STATS_ID)
+        })
     }
 
 }
