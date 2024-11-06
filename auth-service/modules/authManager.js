4014cedb530ecaf20691cd514572cb8f89126839
JVViacrusis <justin.viacrusis21p@gmail.com>
2024-11-05 16:57:18 -0800

    changed username to email since we use emails to log in, not usernames

--- auth-service/modules/authManager.js
+++ auth-service/modules/authManager.js
@@ -7,23 +7,23 @@ module.exports = class AuthManager {
         this.EXPIRATION = '60s'
     }
 
-    isValidUser(username, password) {
+    isValidUser(email, password) {
         // make this a db call eventually
-        if (username === 'admin' || password === '111') {
+        if (email === 'admin' || password === '111') {
             return true
         }
         return false
     }
 
     handleLogin(req, res) {
-        const { username, password } = req.body;
+        const { email, password } = req.body;
 
-        if (!this.isValidUser(username, password)) {
+        if (!this.isValidUser(email, password)) {
             return res.status(401).json({ message: 'Unauthorized' });
         }
 
         console.log(this.SECRET_KEY)
-        const token = jwt.sign({ username }, this.SECRET_KEY, { expiresIn: this.EXPIRATION });
+        const token = jwt.sign({ email }, this.SECRET_KEY, { expiresIn: this.EXPIRATION });
         res.cookie('token', token, { httpOnly: true });
         res.status(200).json({ message: 'Logged in successfully' });
     }
