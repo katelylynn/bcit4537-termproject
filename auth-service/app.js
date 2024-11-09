8c3b8a4ae608fc7c0e58c0fc0d6e7b89c4e74eae
JVViacrusis <justin.viacrusis21p@gmail.com>
2024-11-09 11:06:31 -0800

    refactored auth-service to start on port defined in .env

--- auth-service/app.js
+++ auth-service/app.js
@@ -1,4 +1,5 @@
 const AuthService = require('./modules/api.js');
+require("dotenv").config()
 
 const authService = new AuthService('totallynotakey');
-authService.start(3000);
+authService.start(process.env["PORT"]);
