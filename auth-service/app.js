cce4fd2ca22eb58f1cbfdde60a9af01d8d652950
JVViacrusis <justin.viacrusis21p@gmail.com>
2024-11-10 10:50:58 -0800

    moved jwt key to .env for auth and router services

--- auth-service/app.js
+++ auth-service/app.js
@@ -1,5 +1,5 @@
 const AuthService = require('./modules/api.js');
 require("dotenv").config()
 
-const authService = new AuthService('totallynotakey');
+const authService = new AuthService(process.env['JWT_SECRET_KEY']);
 authService.start(process.env["PORT"]);
