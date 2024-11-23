5e6ae24fcb67bcecefeb0767252eb82662b460bf
kate <katezraly@gmail.com>
2024-11-23 11:02:51 -0800

    added guard for db disconnection

--- database-service/modules/db.js
+++ database-service/modules/db.js
@@ -3,15 +3,30 @@ const mysql = require("mysql2")
 module.exports = class DatabaseConnection {
 
     constructor(host, user, password, database = null) {
-        this.con = mysql.createConnection({
+        this.config = {
             host: host,
             user: user,
             password: password,
             database: database,
-        })
+        }
+        this.connect()
+    }
+
+    connect() {
+        this.con = mysql.createConnection(this.config)
+    
         this.con.connect((err) => {
             if (err) throw err
             console.log("Connected to DB")
+        })
+    
+        this.con.on("error", (err) => {
+            if (err.code === "PROTOCOL_CONNECTION_LOST") {
+                console.log("Connection lost. Reconnecting...")
+                this.connect()
+            } else {
+                console.error("Database error:", err)
+            }
         })
     }
 
